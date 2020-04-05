1715ea421bf7975cb013691375dd864dfe8c4309
Jan Heiland <jnhlnd@gmail.com>
2020-04-05 09:18:20 +0200

    upd mkd

--- makethedoc.sh
+++ makethedoc.sh
@@ -5,7 +5,7 @@ pandoc $MDFILE -o index.html \
     --mathjax='node_modules/mathjax/es5/tex-mml-chtml.js' \
     -V revealjs-url=https://revealjs.com \
     -V theme=solarized -V hash=true \
-    -V viewDistance=15 -V width=1280 -V height=960 -V margin=0.05
+    -V viewDistance=15 -V width=1280 -V height=880 -V margin=0.05
     # -V revealjs-url=node_modules/reveal.js \
     # --mathjax='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' \
 
